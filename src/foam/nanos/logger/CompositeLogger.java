96e6ecda6096bb1523b6e3ec842808dcc2b57bf6
Adam Kenneweg <akenne@users.noreply.github.com>
2017-06-28 14:30:57 -0400

    Composite Logger

---
+++ src/foam/nanos/logger/CompositeLogger.java
@@ -0,0 +1,74 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.nanos.logger;
+
+import foam.nanos.logger.FileLogger;
+import foam.nanos.logger.StdoutLogger;
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.logging.*;
+import java.io.IOException;
+
+public class CompositeLogger
+{
+  public static void main(String args[]){
+    CompositeLogger comp = new CompositeLogger();
+    StdoutLogger test1234 = new StdoutLogger();
+    FileLogger test12345 = new FileLogger();
+    test1234.start();
+    test12345.start();
+    comp.add(test1234).add(test12345);
+    comp.log("yoooo");
+    comp.info("yoooo");
+    comp.warning("yoooo");
+    comp.error("yoooo");
+    comp.debug("yoooo");
+    comp.remove(test1234);
+    comp.remove(test12345);
+  }
+  
+  private List<Logger> childLoggers = new ArrayList<Logger>();
+
+  public CompositeLogger add(Logger logger) {
+    this.childLoggers.add(logger);
+    return this;
+  }
+
+  public void remove(Logger logger) {
+    childLoggers.remove(logger);
+  }
+
+  public void log(Object... args) {
+    for (Logger logger : childLoggers) {
+      logger.info(args);
+    };
+  }
+
+  public void info(Object... args) {
+    for (Logger logger : childLoggers) {
+      logger.info(args);
+    };
+  }
+
+  public void warning(Object... args) {
+    for (Logger logger : childLoggers) {
+      logger.warning(args);
+    };
+  }
+
+  public void error(Object... args) {
+    for (Logger logger : childLoggers) {
+      logger.error(args);
+    };
+  }
+
+  public void debug(Object...  args) {
+    for (Logger logger : childLoggers) {
+      logger.debug(args);
+    };
+  }
+}
--- src/foam/nanos/logger/StdoutLogger.java
+++ src/foam/nanos/logger/StdoutLogger.java
@@ -6,18 +6,16 @@
 
 package foam.nanos.logger;
 
-import foam.core.*;
-import foam.nanos.NanoService;
+import foam.nanos.logger.Logger;
 import java.util.logging.*;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
 
-public class NanoLogger
-  extends    ContextAwareSupport
-  implements NanoService
+public class StdoutLogger
+  implements Logger
 {
-  protected Logger logger;
+  protected java.util.logging.Logger logger;
 
   private static final ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<SimpleDateFormat>() {
     @Override
@@ -41,17 +39,13 @@ public class NanoLogger
   };
 
   public void start() {
-    logger = Logger.getAnonymousLogger();
+    logger = java.util.logging.Logger.getAnonymousLogger();
     logger.setUseParentHandlers(false);
     logger.setLevel(Level.ALL);
-
-    try {
-      Handler handler = new FileHandler("nano.log");
-      handler.setFormatter(new CustomFormatter());
-      logger.addHandler(handler);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    Handler handler = new ConsoleHandler();
+    handler.setLevel(Level.ALL);
+    handler.setFormatter(new CustomFormatter());
+    logger.addHandler(handler);
   }
 
   private class CustomFormatter extends Formatter {
@@ -73,7 +67,7 @@ public class NanoLogger
       str.append(',');
 
       // debug special case, fine level == 500
-      if (lev == 500) {
+      if ( lev == 500 ) {
         str.append("DEBUG");
       } else {
         str.append(record.getLevel());
@@ -87,7 +81,7 @@ public class NanoLogger
 
   public String combine(Object[] args) {
     StringBuilder str = sb.get();
-    for (Object n : args) {
+    for ( Object n : args ) {
       str.append(',');
       str.append(n.toString());
     }
